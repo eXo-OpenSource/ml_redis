69aa4708de73df46fc1b0d70e9d6b6e9a4aa1821
Stefan Kürzeder <stefan.kuerzeder@whiteduck.de>
2019-07-27 15:49:13 +0200

    Added RedisClient wrapper

---
+++ module/RedisClient.cpp
@@ -0,0 +1,35 @@
+﻿#include "RedisClient.h"
+#include "Module.h"
+
+RedisClient::RedisClient()
+{
+	_client = new redis_client();
+	_subscriber = new redis_subscriber();
+
+	g_Module->AddRedisClient(this);
+}
+
+RedisClient::~RedisClient()
+{
+	g_Module->RemoveRedisClient(this);
+
+	delete _client;
+	delete _subscriber;
+}
+
+void RedisClient::connect(const std::string& host, const int& port) const
+{
+	_client->connect(host, port);
+	_subscriber->connect(host, port);
+}
+
+void RedisClient::disconnect() const
+{
+	_client->disconnect(true);
+	_subscriber->disconnect(true);
+}
+
+bool RedisClient::is_connected() const
+{
+	return _client->is_connected() && _subscriber->is_connected();
+}
---
+++ module/RedisClient.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <cpp_redis/core/client.hpp>
+#include <cpp_redis/core/subscriber.hpp>
+
+using redis_client = cpp_redis::client;
+using redis_subscriber = cpp_redis::subscriber;
+
+class RedisClient
+{
+public:
+	RedisClient();
+	~RedisClient();
+
+	void connect(const std::string& host, const int& port) const;
+	void disconnect() const;
+	bool is_connected() const;
+private:
+	redis_client* _client;
+	redis_subscriber* _subscriber;
+};
+
