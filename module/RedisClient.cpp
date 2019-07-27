7571810442dffb2da90f6762e0695f4aa74b2f4a
Stefan Kürzeder <stefan.kuerzeder@whiteduck.de>
2019-07-27 17:09:32 +0200

    Inlined some functions

--- module/RedisClient.cpp
+++ module/RedisClient.cpp
@@ -17,23 +17,6 @@ redis_client::~redis_client()
 	delete _subscriber;
 }
 
-void redis_client::connect(const std::string& host, const int& port) const
-{
-	_client->connect(host, port);
-	_subscriber->connect(host, port);
-}
-
-void redis_client::disconnect() const
-{
-	_client->disconnect(true);
-	_subscriber->disconnect(true);
-}
-
-bool redis_client::is_connected() const
-{
-	return _client->is_connected() && _subscriber->is_connected();
-}
-
 cpp_redis::reply redis_client::set(const std::string& key, const std::string& value) const
 {
 	auto result = _client->set(key, value);
@@ -41,13 +24,6 @@ cpp_redis::reply redis_client::set(const std::string& key, const std::string& va
 
 	result.wait();
 	return result.get();
-}
-
-void redis_client::set(const std::string& key, const std::string& value,
-	const cpp_redis::client::reply_callback_t& callback) const
-{
-	_client->set(key, value, callback);
-	_client->commit();
 }
 
 void redis_client::set(const std::map<std::string, std::string>& pairs, const std::function<void(const std::string&, cpp_redis::reply &)>& callback) const
@@ -69,12 +45,6 @@ cpp_redis::reply redis_client::get(const std::string& key) const
 
 	result.wait();
 	return result.get();
-}
-
-void redis_client::get(const std::string& key, const cpp_redis::client::reply_callback_t& callback) const
-{
-	_client->get(key, callback);
-	_client->commit();
 }
 
 void redis_client::get(const std::list<std::string>& keys, const std::function<void(const std::string&, cpp_redis::reply &)>& callback) const
--- module/RedisClient.h
+++ module/RedisClient.h
@@ -8,16 +8,16 @@ public:
 	redis_client();
 	~redis_client();
 
-	void connect(const std::string& host, const int& port) const;
-	void disconnect() const;
-	[[nodiscard]] bool is_connected() const;
+	void connect(const std::string& host, const int& port) const { _client->connect(host, port); _subscriber->connect(host, port); }
+	void disconnect() const { _client->disconnect(); _subscriber->disconnect(); }
+	[[nodiscard]] bool is_connected() const { return _client->is_connected() && _subscriber->is_connected(); }
 
 	[[nodiscard]] cpp_redis::reply set(const std::string& key, const std::string& value) const;
-	void set(const std::string& key, const std::string& value, const cpp_redis::client::reply_callback_t& callback) const;
+	void set(const std::string& key, const std::string& value, const cpp_redis::client::reply_callback_t& callback) const { _client->set(key, value, callback); _client->commit(); }
 	void set(const std::map<std::string, std::string>& pairs, const std::function<void(const std::string&, cpp_redis::reply &)>& callback) const;
 
 	[[nodiscard]] cpp_redis::reply get(const std::string& key) const;
-	void get(const std::string& key, const cpp_redis::client::reply_callback_t& callback) const;
+	void get(const std::string& key, const cpp_redis::client::reply_callback_t& callback) const { _client->get(key, callback); _client->commit(); };
 	void get(const std::list<std::string>& keys, const std::function<void(const std::string&, cpp_redis::reply &)>& callback) const;
 private:
 	cpp_redis::client* _client;
